dd15bbfc61f60f84f690717248d6f1da20153d96
jonayeds <jonayeeds@gmail.com>
2024-07-10 21:53:26 +0600

    handled like frontend

--- src/pages/PostDetails/PostDetails.jsx
+++ src/pages/PostDetails/PostDetails.jsx
@@ -1,14 +1,26 @@
+import { useState } from "react";
 import { CiHeart } from "react-icons/ci";
 import { GoComment } from "react-icons/go";
 import { useLoaderData, useParams } from "react-router-dom";
-
-
+import { FaHeart } from "react-icons/fa";
+import useAxiosSecure from "../../custom hooks/useAxiosSecure";
 
 const PostDetails = () => {
     const user = useLoaderData()
     const param = useParams()
     const postId = param.id
     const post = user.posts.find(post=> post.postId  === parseInt(postId))
+    const [like,  setLike] =  useState(false)
+    const axiosSecure =   useAxiosSecure()
+    console.log(user)
+    const handleLike = ()=>{
+        setLike(!like)
+        if(!like){
+            axiosSecure.put(`/post/update/${user.email}/${postId}`, {    
+                
+            })
+        }
+        }
     return (
         <div className="flex flex-col md:w-max  mx-auto pt-16 px-4">
             <div className="flex items-center gap-4  mb-8">
@@ -21,12 +33,17 @@ const PostDetails = () => {
             <p className="mt-4 text-gray-300 mb-6">{post.caption}</p>
             <hr className="border border-gray-500" />
             <div>
-            <div className="flex justify-around    pt-8 ">
-            <CiHeart className="w-full border-r border-gray-600 text-4xl text-white "  />
-            <GoComment className="w-full text-white text-3xl" />
+            <div className="flex justify-around     pt-8 ">
+            <div className="w-full border-r border-gray-600 flex justify-center">
+                {
+                    like? <FaHeart className="text-3xl text-red-500 cursor-pointer" onClick={handleLike}   />:  <CiHeart className=" cursor-pointer  text-4xl text-white " onClick={handleLike}  />
+                }
+            </div>
+            <div className="w-full  flex justify-center">
+            <GoComment className=" text-white text-3xl  cursor-pointer" />
             </div>
             </div>
-
+            </div>
         </div>
     );
 };
