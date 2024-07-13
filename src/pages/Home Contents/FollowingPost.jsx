42a077a3a2073926bf72afac83bf900fd7bfb283
jonayeds <jonayeeds@gmail.com>
2024-07-13 13:03:53 +0600

    made feed layout

--- src/pages/Home Contents/FollowingPost.jsx
+++ src/pages/Home Contents/FollowingPost.jsx
@@ -1,8 +1,105 @@
+/* eslint-disable react/prop-types */
 
-const FollowingPost = () => {
+import { useEffect, useState } from "react";
+import { CiHeart } from "react-icons/ci";
+import { FaHeart } from "react-icons/fa";
+import { GoComment } from "react-icons/go";
+import { Link } from "react-router-dom";
+import useAxiosSecure from "../../custom hooks/useAxiosSecure";
+
+const FollowingPost = ({post, author, currentUser}) => {
+    const [like, setLike] = useState(false);
+    const [likeCount, setLikeCount] = useState(post.likes);
+    const [comments, setComments] = useState([]);
+    const axiosSecure = useAxiosSecure();
+    const posts = author.posts
+    
+      const idx = author.posts.indexOf(post);
+      useEffect(() => {
+        axiosSecure
+          .get(`/comments/${author.email}/${post.postId}`)
+          .then((commentsData) => {
+            console.log("comments", commentsData.data);
+            setComments(commentsData.data);
+          });
+      }, [author, post, axiosSecure]);
+      useEffect(() => {
+        const liked = post.likeAccounts.filter(
+            (liker) => liker === currentUser?.email
+          );
+        setLike(liked.length ? true : false);
+        console.log(liked)
+      }, [currentUser, post]);
+      const handleLike = () => {
+        setLike(!like);
+    
+        if (!like) {
+          posts[idx] = {
+            ...post,
+            likes: post.likes + 1,
+            likeAccounts: [...post.likeAccounts, currentUser.email],
+          };
+          axiosSecure
+            .put(`/post/update/${author.email}`, {
+              posts: posts,
+            })
+            .then((res) => {
+              console.log(res.data);
+              setLikeCount(likeCount + 1);
+            });
+        } else {
+          posts[idx] = {
+            ...post,
+            likes: post.likes - 1,
+            likeAccounts: post.likeAccounts.filter(
+              (account) => account !== currentUser.email
+            ),
+          };
+          axiosSecure
+            .put(`/post/update/${author.email}`, {
+              posts: posts,
+            })
+            .then((res) => {
+              console.log(res.data);
+              setLikeCount(likeCount - 1);
+            });
+        }
+      };
     return (
         <div>
-            
+            <div className="flex items-center">
+                <div className="w-10 h-10 overflow-hidden rounded-full border-white border-[3px] hover:border-[#aabbff] duration-500">
+                <img src={author.image} alt="" className="w-10 " />
+                </div>
+                <Link to={`/search/${author._id}` } className="ml-2 text-white font-semibold ">{author.fullName}</Link>
+            </div>
+            <Link to={`/post/${author.email}/${post.postId}`}>
+                <img src={post.uploadImg} className="md:max-w-md w-full rounded-xl mt-4" alt="" />
+            </Link>
+            <p className="mt-4 text-gray-300 mb-6">{post.caption}</p>
+      <hr className="border border-gray-500" />
+      <div>
+        <div className="flex justify-around     pt-8 ">
+          <div className="w-full border-r border-gray-600 flex  items-center justify-center">
+            {like ? (
+              <FaHeart
+                className="md:text-3xl text-2xl text-red-500 cursor-pointer"
+                onClick={handleLike}
+              />
+            ) : (
+              <CiHeart
+                className=" cursor-pointer  md:text-4xl text-3xl text-white "
+                onClick={handleLike}
+              />
+            )}
+            <p className="ml-3 text-xl ">{likeCount ? likeCount : ""}</p>
+          </div>
+          <Link to={`/post/${author.email}/${post.postId}`} className="w-full  flex justify-center">
+            <GoComment className=" text-white md:text-3xl text-2xl  cursor-pointer" />
+            <p className="ml-2">{comments.length ? comments.length : ""}</p>
+          </Link>
+        </div>
+      </div>
         </div>
     );
 };
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -11,7 +11,6 @@ const Home = () => {
     console.log(allUsers)
     const [user, setUser] = useState({})
     const [following, setFollowing] = useState([])
-    const [posts, setPosts] = useState([])
     const axiosSecure = useAxiosSecure()
     useEffect(()=>{
         axiosSecure.get(`users/email/${currentUser?.email}`)
@@ -20,23 +19,26 @@ const Home = () => {
            setFollowing( allUsers.filter(aUser=> data.data.followingAccounts.includes(aUser.email)))
         })
     }, [currentUser, axiosSecure, allUsers])
-    useEffect(()=>{
-        for(let i = 0; i<following.length; i++){
-            for(let j =0; j<following[i].posts.length; j++){
-                posts.push(following[i].posts[j])
-            }
+    // useEffect(()=>{
+    //     for(let i = 0; i<following.length; i++){
+    //         for(let j =0; j<following[i].posts.length; j++){
+    //             posts.push(following[i].posts[j])
+    //         }
             
-        }
-    //    following.map(f=>setPosts([...f.posts, ...posts])) 
-    },[following])
-    console.log('posts',posts.length)
+    //     }
+    // //    following.map(f=>setPosts([...f.posts, ...posts])) 
+    // },[following])
     console.log('following accounts',following)
     return (
         <div>
             {
-                following.map(f=> f.posts.map(post=> <div key={post.postId + post.currentHours + post.currentDate}>
-                    <p>1</p>
-                 </div>))
+                following.map(f=> <div className="pt-16" key={f._id}>
+                    {
+                         f.posts.map(post=> <div className="w-full flex flex-col items-center px-4" key={post.postId + post.currentHours + post.currentDate}>
+                            <FollowingPost post={post} currentUser={user} author={f}></FollowingPost>
+                         </div>)
+                    }
+                </div>)
             }
         </div>
     );
